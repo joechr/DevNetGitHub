3f9b6e82626f69e70b83820035716b73effeee9b
Joe Christensen <joe-christensen@outlook.com>
2020-03-17 17:18:39 -0500

    Change files

--- sample.js
+++ sample.js
@@ -1,11 +1,13 @@
+myObj = {"People":
 [
-    {
+            {
                 "Id": 1,
                 "FirstName": "Joe",
                 "LastName": "Christensen",
                 "Email": "joe.christensen@siriuscom.com",
                 "Active" : true
-    }
+            }
 ]
+}
 
 console.log(typeof myObj.People[0].Active);
